a2d772512bcbd36082ba44b6da5d774a46987ea5
Pham Quang Hoa <20520995@gm.uit.edu.vn>
2022-11-26 00:40:29 +0700

    init3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { Grid, Box, Container } from "@mui/material";
+import { ethers } from "ethers";
+// import MarketplaceAbi from "./utils/contractsData/Marketplace.json";
+// import MarketplaceAddress from "./utils/contractsData/Marketplace-address.json";
+// import NFTAbi from "./utils/contractsData/NFT.json";
+// import NFTAddress from "./utils/contractsData/NFT-address.json";
 
-function App() {
+import "./App.css";
+import AppBar from "./components/Appbar/AppBar";
+import Landing from "./pages/Landing/Landing";
+
+const App = () => {
+ 
   return (
-    <div className="App">
-     This is an app
-    </div>
+    <BrowserRouter>
+    <Box className="app" >
+      <AppBar/>
+      <Box className="container" >
+        <Container className="pages" maxWidth="xl"  >
+        <Switch>
+        <Route path="/" exact component={Landing} />
+        </Switch>
+
+        </Container>
+      </Box>
+      </Box>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
